b74e28b6a40d84b9a9ae763b23c3c005a3762916
vinitparmar <vinitparmar89@gmail.com>
2025-09-24 21:38:40 +0530

    Naive Pattern Searching algorithm

--- string/java/PatternMatching/Naive.java
+++ string/java/PatternMatching/Naive.java
@@ -1,5 +1,33 @@
 package string.java.PatternMatching;
 
 public class Naive {
-    
+ 
+    static void search(String text, String pattern){
+
+        int n = text.length();
+        int m = pattern.length();
+
+        int i = 0;
+
+        while (i <= n-m) {
+            if(text.substring(i, i+m).equals(pattern)){
+                System.out.println("patern found at index "+i);
+            }
+            i++;
+        }
+
 }
+
+public static void main(String[] args) {
+    String text = "ABABABCABABABCABAB";
+    String pattern = "ABAB";
+    search(text, pattern);
+}
+
+}
+
+/*
+* Complexity
+* Time : O(n)
+* Space : O(n)
+*/
