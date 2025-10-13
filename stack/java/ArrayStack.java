230fb547e9a527b0f1b889ad5acd3d0bf93dfa00
vinitparmar <vinitparmar89@gmail.com>
2025-10-13 23:11:07 +0530

    stack implementation

---
+++ stack/java/ArrayStack.java
@@ -0,0 +1,62 @@
+package stack.java;
+
+import java.util.Arrays;
+
+public class ArrayStack {
+
+    private int maxSize;
+    private int[] stackArray;
+    private int top;
+
+    public ArrayStack(int size) {
+        this.maxSize = size;
+        this.stackArray = new int[maxSize];
+        this.top = -1;
+    }
+
+    public void push(int value) {
+        if (isFull()) {
+            System.out.println("Cannot push — Stack is full!");
+            return;
+        }
+        stackArray[++top] = value;
+        System.out.println(value + " pushed to stack.");
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Cannot pop — Stack is empty!");
+            return -1;
+        }
+        int poppedVal = stackArray[top--];
+        System.out.println(poppedVal + " popped from stack.");
+        return poppedVal;
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty.");
+            return -1;
+        }
+        return stackArray[top];
+    }
+
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+
+    public boolean isFull() {
+        return (top == maxSize - 1);
+
+    }
+   public static void main(String[] args) {
+        ArrayStack stack = new ArrayStack(5);
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println("Top element: " + stack.peek());
+
+       stack.pop();
+    }
+}
