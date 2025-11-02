08002b4f6f8abc860a2d89ed85637f2f4b8230ce
vinitparmar <vinitparmar89@gmail.com>
2025-11-02 16:22:45 +0530

    linear search

--- recursion/java/LinearSearch.java
+++ recursion/java/LinearSearch.java
@@ -10,10 +10,23 @@ public class LinearSearch {
         return arr[index] == target || search(arr, target, index+1);
 
     }
+
+    static int findIndex(int[] arr , int target , int index){
+        if(index == arr.length){
+            return -1;
+        }
+
+        if(arr[index] == target){
+            return index;
+        }
+
+        return findIndex(arr, target, index+1);
+    }
     
     public static void main(String[] args) {
          int[] arr = {1,2,3,5,4,7};
          int target = 7;
         System.out.println(search(arr, target, 0));
+        System.out.println(findIndex(arr, target, 0));
     }
 }
